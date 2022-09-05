1da60de2bfcf0aa776f9f39361c7ec4a43f66795
Isabel <isanpe@gmail.com>
2022-09-05 19:40:20 -0300

    refactoriza codigo controlador cart y modelo order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,23 @@
+belongs_to :user
+
 class Order < ApplicationRecord
+  
+  #...
+  def total_in_cents
+  total * 100
+  end
+  
+  def create_payment(pm_code, token)
+  payments << Payment.create(
+  payment_method_id: PaymentMethod.find_by(code: pm_code).id,
+  state: "processing",
+  total: total,
+  token: token
+  )
+  end
+  
+  def complete!
+  update_attributes({state: "completed"})
+  end
 end
+
